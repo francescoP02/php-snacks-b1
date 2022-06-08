de65a4bae2f70e034529732831a2ea8a0f176474
francescoP02 <file.partipilo@gmail.com>
2022-06-08 16:41:50 +0200

    added snack3

--- snack3/index.php
+++ snack3/index.php
@@ -14,6 +14,75 @@ error_reporting(E_ALL);
 </head>
 <body>
 
+<!-- Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post. -->
+
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+?>
+
+<ul>
+    <?php 
+    for ($i = 0; $i < count(array_keys($posts)); $i++) {
+      $day_post = array_keys($posts)[$i];
+    ?>
+      <li>
+        <h3>Post del <?php echo $day_post; ?></h3>
+        <?php for ($j = 0 ; $j < count($posts[$day_post]) ; $j++ ) { 
+            $post = $posts[$day_post][$j];
+        ?>
+        <ul>
+            <li>
+                <h5><?php echo $post["title"]; ?></h5>
+                <span><?php echo $post["author"]; ?></span>
+                <p><?php echo $post["text"]; ?></p>
+            </li>
+        </ul>
+        <?php } ?>
+      </li>
+    <?php } ?>
+
+</ul>
+
     
 </body>
 </html>
